26199b09a44566e8bf11bb3cc9e22c8f678baffb
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-04-09 23:07:57 +0200

    magic

--- TreningGenerator/Exercises.swift
+++ TreningGenerator/Exercises.swift
@@ -10,12 +10,14 @@ import Foundation
 
 
 struct Exercises {
+    // https://github.com/apple/swift-evolution/blob/master/proposals/0194-derived-collection-of-enum-cases.md
     // Znaleźć sposób żeby przerobić enum na tablice automatycznie
     
     let chestTricepsShouldersArray = [ChestTricepsShoulders.a1.rawValue, ChestTricepsShoulders.a2.rawValue, ChestTricepsShoulders.a3.rawValue, ChestTricepsShoulders.a4.rawValue]
     let backBicepsArray = [BackBiceps.b1.rawValue, BackBiceps.b2.rawValue, BackBiceps.b3.rawValue, BackBiceps.b4.rawValue]
     let legsArray = [Legs.c1.rawValue, Legs.c2.rawValue, Legs.c3.rawValue, Legs.c4.rawValue]
     let absArray = [Abs.d1.rawValue, Abs.d2.rawValue, Abs.d3.rawValue, Abs.d4.rawValue]
+    
     
     func randomExercise(_ anyExerciseArray:[String]) -> Int {
         let random = Int(arc4random_uniform(UInt32(anyExerciseArray.count)))
