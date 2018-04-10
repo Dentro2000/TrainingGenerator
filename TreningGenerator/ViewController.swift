02e5c0fe89c55af697a977b2eb445d6c92bff966
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-04-10 18:44:21 +0200

    Makes code more condensed.

--- TreningGenerator/ViewController.swift
+++ TreningGenerator/ViewController.swift
@@ -22,36 +22,21 @@ class ViewController: UIViewController {
         //let arrayOfArrays = [forChest, forAbs, forBack, forLegs]
         let arrayOfLables = [ex1,ex2, ex3, ex4, ex5, ex6, ex7, ex8].compactMap{ $0 }
 
-//        let exerciseForChest =
-//        ex1.text = exerciseForChest[0]
-//        ex2.text = exerciseForChest[1]
-
-//        let exeriseForAbs = exercises.randomExerciseGenerator(AbsExercises().exercises.map { $0.name })
-//        ex3.text = exeriseForAbs[0]
-//        ex4.text = exeriseForAbs[1]
-//
-//        let exeriseForBack = exercises.randomExerciseGenerator(forBack)
-//        ex5.text = exeriseForBack[0]
-//        ex6.text = exeriseForBack[1]
-//
-//        let exeriseForLegs = exercises.randomExerciseGenerator(forLegs)
-//        ex7.text = exeriseForLegs[0]
-//        ex8.text = exeriseForLegs[1]
 
         let allExecises: [String] = [
             exercises.randomExerciseGenerator(forChest),
             exercises.randomExerciseGenerator(AbsExercises().exercises.map { $0.name }),
             exercises.randomExerciseGenerator(forBack),
             exercises.randomExerciseGenerator(forLegs)
-            ]
+            ].flatMap{ $0 }
 
         let zipped = zip(arrayOfLables, allExecises)
 
-//        zipped.forEach { (arg) in
-//
-//            let (label, chars) = arg
-//            label.text = String(chars)
-//        }
+        zipped.forEach { (arg) in
+
+            let (label, chars) = arg
+            label.text = String(chars)
+        }
 
     }
     
