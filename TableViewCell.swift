845c14cb3c4ace49cd5cff40c915bbc97aa03a5e
Dentro2000 <szymon.a.kowalski@allegrogroup.com>
2018-04-11 22:32:28 +0200

    .

---
+++ TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  TreningGenerator
+//
+//  Created by SZK on 11.04.2018.
+//  Copyright © 2018 SZK. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var exLabel: UILabel?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TreningGenerator/ViewController.swift
+++ TreningGenerator/ViewController.swift
@@ -1,27 +1,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    @IBOutlet weak var ex1: UILabel!
-    @IBOutlet weak var ex2: UILabel!
-    @IBOutlet weak var ex3: UILabel!
-    @IBOutlet weak var ex4: UILabel!
-    @IBOutlet weak var ex5: UILabel!
-    @IBOutlet weak var ex6: UILabel!
-    @IBOutlet weak var ex7: UILabel!
-    @IBOutlet weak var ex8: UILabel!
-    
+class ViewController: UITableViewController {
     
     let exercises = ExercisesModel()
-    
-    
-    @IBAction func generateTraining() {
-        let arrayOfLables = [ex1,ex2, ex3, ex4, ex5, ex6, ex7, ex8].compactMap{ $0 }
-        
-        zip(arrayOfLables, exercises.workoutPlan).forEach { (arg) in
-            let (label, exercise) = arg
-            label.text = exercise.name
-        }
+   
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return exercises.workoutPlan.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "exCell", for: indexPath) as! TableViewCell
+        
+        let workout = exercises.workoutPlan as! [String]
+        for exercise in workout {
+            cell.exLabel?.text = exercise
+        }
+        
+        
+        return cell
+    }
+    
+   
+    
 }
+//@IBAction func generateTraining() {
+//    let arrayOfLables = [ex1,ex2, ex3, ex4, ex5, ex6, ex7, ex8].compactMap{ $0 }
+//
+//    zip(arrayOfLables, exercises.workoutPlan).forEach { (arg) in
+//        let (label, exercise) = arg
+//        label.text = exercise.name
+//    }}
