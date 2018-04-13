76529cc16f19bdc7c8c6d800c8ac024a5b1d5cd3
Dentro2000 <szymon.a.kowalski@allegrogroup.com>
2018-04-13 22:00:48 +0200

    .

--- TreningGenerator/ViewController.swift
+++ TreningGenerator/ViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 
 struct TraningGenerator {
     let exeModel: ExercisesModel
-
+    
 
     var workoutPlan: [String] {
         func takeRandom(_ inArray: Array<Any>) -> [ExerciseProtocol] {
@@ -12,13 +12,13 @@ struct TraningGenerator {
         }
 
         let workout = takeRandom(exeModel.abs) + takeRandom(exeModel.chestTricepsShoulders) + takeRandom(exeModel.backBiceps) + takeRandom(exeModel.legs)
-
         return workout.map {$0.name}
     }
-
+    
 }
 
 class ViewController: UITableViewController {
+    let sections = ["AbsExercises", "ChestTricepsShouldersExercises", "BackBicepsExercises", "LegsExercises"]
 
     lazy var traningGenerator = {
         return TraningGenerator(exeModel: model)
@@ -38,21 +38,34 @@ class ViewController: UITableViewController {
         tableView.reloadData()
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return workout.count
+        return workout.count/sections.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "exCell", for: indexPath) as! TableViewCell
         
-        
+
         cell.exLabel?.text = workout[indexPath.row]
         return cell
     }
     
     override func tableView(_ tableView:    UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
     }
 }
 
