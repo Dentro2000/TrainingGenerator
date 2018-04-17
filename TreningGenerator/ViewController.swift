16dd189989615e19fef88295a0f0c4253c53a10b
Lukasz Stocki <lukasz.stocki@allegro.pl>
2018-04-17 11:01:42 +0200

    WiP: refactors stuff

--- TreningGenerator/ViewController.swift
+++ TreningGenerator/ViewController.swift
@@ -3,33 +3,45 @@ import UIKit
 
 struct TraningGenerator {
     let exeModel: ExercisesModel
-    
 
-    var workoutPlan: [String] {
-        func takeRandom(_ inArray: Array<Any>) -> [ExerciseProtocol] {
-            let array = (inArray as NSArray).shuffled().prefix(2)
-            return Array(array) as! [ExerciseProtocol]
+    struct WorkautPlan {
+
+        struct Set {
+
+            let header: String
+            let exes: [String]
         }
 
-        let workout = takeRandom(exeModel.abs) + takeRandom(exeModel.chestTricepsShoulders) + takeRandom(exeModel.backBiceps) + takeRandom(exeModel.legs)
-        return workout.map {$0.name}
+        let sets: [TraningGenerator.WorkautPlan.Set]
     }
-    
+
+    var workoutPlan: WorkautPlan {
+        func takeRandom(_ inArray: Array<Any>) -> [String] {
+            let array = (inArray as NSArray).shuffled().prefix(2)
+            let exe = Array(array) as! [ExerciseProtocol]
+            return exe.map { $0.name }
+        }
+
+        return WorkautPlan(sets: [
+            TraningGenerator.WorkautPlan.Set(header: "AbsExercises",    exes: takeRandom(exeModel.abs)),
+            TraningGenerator.WorkautPlan.Set(header: "Back and Biceps", exes: takeRandom(exeModel.backBiceps))
+            ])
+    }
 }
 
+
 class ViewController: UITableViewController {
-    let sections = ["AbsExercises", "ChestTricepsShouldersExercises", "BackBicepsExercises", "LegsExercises"]
 
     lazy var traningGenerator = {
         return TraningGenerator(exeModel: model)
     }()
 
-    var model   = ExercisesModel()
-    lazy var workout: [String] = {
+    var model = ExercisesModel()
+    lazy var workout: TraningGenerator.WorkautPlan = {
         return generateTrainig()
     }()
 
-    func generateTrainig() -> [String] {
+    func generateTrainig() -> TraningGenerator.WorkautPlan {
         return traningGenerator.workoutPlan
     }
 
@@ -39,23 +51,21 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section]
+        return workout.sets[section].header
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return sections.count
+        return workout.sets.count
     }
     
-    
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return workout.count/sections.count
+        return workout.sets[section].exes.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "exCell", for: indexPath) as! TableViewCell
    
-        cell.exLabel?.text = workout[indexPath.row]
+        cell.exLabel?.text = workout.sets[indexPath.section].exes[indexPath.row]
         return cell
     }
     
