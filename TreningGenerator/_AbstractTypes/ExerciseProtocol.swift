d04d8d3eda640ebd10b8d6730260d67bc5372f8c
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-04-19 00:03:22 +0200

    Adds a requirement on exercise so that it has "tags" of kinds of exercises.

--- TreningGenerator/_AbstractTypes/ExerciseProtocol.swift
+++ TreningGenerator/_AbstractTypes/ExerciseProtocol.swift
@@ -1,8 +1,13 @@
 
 import Foundation
 
+enum ExerciseKind: Int {
+    case calisthenics
+    case streching
+}
+
 protocol ExerciseProtocol {
     var name: String { get }
     var level: ExerciseLevel { get }
-    var kind: ExerciseKind {get}
+    var kind: Set<ExerciseKind> { get }
 }
