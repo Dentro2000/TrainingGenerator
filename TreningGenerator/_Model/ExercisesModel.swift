7d21d76c27b96b16c47aa0599595d42daadee1c0
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-04-19 00:04:52 +0200

    Tags exercises.
    As a test code abs is a cali and stretching exercise.

--- TreningGenerator/_Model/ExercisesModel.swift
+++ TreningGenerator/_Model/ExercisesModel.swift
@@ -5,37 +5,40 @@ import GameplayKit
 struct ExercisesModel {
     
     let abs: [AbsExerciseProtocol] = [
-        AbsExercise(name: "Spięcia brzucha w leżeniu",              level: .simple, kind: .calisthenics),
-        AbsExercise(name: "Mountain Climbers (Bieg w podporze)",    level: .simple, kind: .calisthenics),
-        AbsExercise(name: "Plank",                                  level: .simple, kind: .calisthenics),
-        AbsExercise(name: "Skręty tułowia z unoszeniem nóg",        level: .simple, kind:. calisthenics),
+        AbsExercise(name: "Spięcia brzucha w leżeniu",              level: .simple, kind: [.calisthenics]),
+        AbsExercise(name: "Mountain Climbers (Bieg w podporze)",    level: .simple, kind: [.calisthenics]),
+        AbsExercise(name: "Plank",                                  level: .simple, kind: [.calisthenics]),
+        AbsExercise(name: "Skręty tułowia z unoszeniem nóg",        level: .simple, kind: [.calisthenics]),
             
-        AbsExercise(name: "Hard Abs 1", level: .hard, kind: .calisthenics)
+        AbsExercise(name: "Hard Abs 1", level: .hard, kind: [.calisthenics, .streching])
         ]
     
     
     let chestTricepsShoulders: [ChestTricepsShouldersExercisesProtocol] = [
-        ChestTricepsShouldersExercises(name: "Pompki Klasyczne", level: .simple, kind: .calisthenics),
-        ChestTricepsShouldersExercises(name: "Popmpki Szerokie", level: .simple, kind: .calisthenics),
-            ChestTricepsShouldersExercises(name: "Pike push-up", level: .simple, kind: .calisthenics),
-            ChestTricepsShouldersExercises(name: "Dipy na krześle", level: .simple, kind: .calisthenics)
+        ChestTricepsShouldersExercises(name: "Pompki Klasyczne", level: .simple, kind: [.calisthenics]),
+        ChestTricepsShouldersExercises(name: "Popmpki Szerokie", level: .simple, kind: [.calisthenics]),
+            ChestTricepsShouldersExercises(name: "Pike push-up", level: .simple, kind: [.calisthenics]),
+            ChestTricepsShouldersExercises(name: "Dipy na krześle", level: .simple, kind: [.calisthenics])
         ]
     
     
     let backBiceps: [BackBicepsExercisesProtocol] =  [
-            BackBicepsExercises(name: "Podciąganie poziome do krawędzi blatu stołu podchwyt",           level: .simple, kind: .calisthenics),
-            BackBicepsExercises(name: "Podciąganie poziome do krawędzi blatu stołu szeroki nachwyt",    level: .simple, kind: .calisthenics),
-            BackBicepsExercises(name: "Podciąganie poziome do krawędzi blatu stołu wąski nachwyt",      level: .simple, kind: .calisthenics),
+            BackBicepsExercises(name: "Podciąganie poziome do krawędzi blatu stołu podchwyt",
+                                level: .simple, kind: [.calisthenics]),
+            BackBicepsExercises(name: "Podciąganie poziome do krawędzi blatu stołu szeroki nachwyt",
+                                level: .simple, kind: [.calisthenics]),
+            BackBicepsExercises(name: "Podciąganie poziome do krawędzi blatu stołu wąski nachwyt",
+                                level: .simple, kind: [.calisthenics]),
             
-            BackBicepsExercises(name: "Superman",   level: .simple, kind: .calisthenics)
+            BackBicepsExercises(name: "Superman",   level: .simple, kind: [.calisthenics])
         ]
     
     
     let legs: [LegsExercisesProtocol] = [
-            LegsExercises(name: "Przysiad / Głęboki przysiad",              level: .simple, kind: .calisthenics),
-            LegsExercises(name: "Wykroki",                                  level: .simple, kind: .calisthenics),
-            LegsExercises(name: "Unoszenie bioder w leżeniu na plecach",    level: .simple, kind: .calisthenics),
-            LegsExercises(name: "Wspięcia na palce jednej nogi",            level: .simple, kind: .calisthenics)
+            LegsExercises(name: "Przysiad / Głęboki przysiad",              level: .simple, kind: [.calisthenics]),
+            LegsExercises(name: "Wykroki",                                  level: .simple, kind: [.calisthenics]),
+            LegsExercises(name: "Unoszenie bioder w leżeniu na plecach",    level: .simple, kind: [.calisthenics]),
+            LegsExercises(name: "Wspięcia na palce jednej nogi",            level: .simple, kind: [.calisthenics])
         ]
     
     
